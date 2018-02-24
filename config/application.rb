b9f7893849bfcde23fabefe4b3f6ec6bf81abb8c
EC2 Default User <ec2-user@ip-172-31-39-140.ap-southeast-1.compute.internal>
2018-02-24 16:37:25 +0000

    init

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module Vicke
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,5 +24,6 @@ module Vicke
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+  
   end
 end
